a55e0549fbe3664f4cd4c1eff833ea89c0b5ba58
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-03 19:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == string.toUpperCase()) {
-    return 
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"; 
+  }
+  else if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 }
